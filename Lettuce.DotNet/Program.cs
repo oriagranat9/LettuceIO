637604474d8db3eea33d1b457b765c7f8cff8970
Unknown <oriagranat9@gmail.com>
2020-08-02 15:39:02 +0300

    added C# proj

---
+++ Lettuce.DotNet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ElectronCgi.DotNet;
+
+namespace Lettuce.DotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = new ConnectionBuilder().WithLogging().Build();
+            
+            connection.Listen();
+        }
+    }
+}
