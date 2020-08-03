1e844a9448695dc99aaecf00b72d860feca603a4
atlasyonatan <atlasyonatan@gmail.com>
2020-08-03 10:21:43 +0300

    fixed solution

---
+++ LettuceIo.Dotnet/LettuceIo.Dotnet.ConsoleHost/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ElectronCgi.DotNet;
+
+namespace LettuceIo.Dotnet.ConsoleHost
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var connection = new ConnectionBuilder().WithLogging().Build();
+            
+            connection.Listen();
+        }
+    }
+}
