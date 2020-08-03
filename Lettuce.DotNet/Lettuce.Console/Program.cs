230095cc1ee7cd061c8b9e1073308a58d448799c
Unknown <oriagranat9@gmail.com>
2020-08-03 09:56:33 +0300

    added connection

--- Lettuce.DotNet/Lettuce.Console/Program.cs
+++ Lettuce.DotNet/Lettuce.Console/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
-
+using ElectronCgi.DotNet;
 namespace Lettuce.ConsoleHost
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var connection = new ConnectionBuilder().WithLogging().Build();
+
+            connection.Listen();
         }
     }
 }
