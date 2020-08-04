5a1f167c42f38707b6183f74bcd99c0e42682c43
atlasyonatan <atlasyonatan@gmail.com>
2020-08-04 11:34:13 +0300

    wip record with reactive

--- LettuceIo.Dotnet/LettuceIo.Dotnet.Base/Actions/Record.cs
+++ LettuceIo.Dotnet/LettuceIo.Dotnet.Base/Actions/Record.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Timers;
 using LettuceIo.Dotnet.Core;
 using Newtonsoft.Json.Linq;
 using RabbitMQ.Client;
@@ -12,34 +15,67 @@ namespace LettuceIo.Dotnet.Base.Actions
         private readonly ILimiter _limiter;
         public string Id { get; set; }
         public IObservable<ActionStats> Stats { get; set; }
-        
+
         public Record(JToken settings)
         {
             _channel = settings.Value<ConnectionFactory>().CreateConnection().CreateModel();
         }
 
+        private string? _consumerTag = null;
+        private IDisposable? _subscription = null;
+        private bool _limitReached = false;
+        private TimeSpan _timeLimit = TimeSpan.FromSeconds(10);
+        private bool _withTimeLimit = false;
+        private bool _withCountLimit = false;
+        private int? _countLimit = null;
+
         public void Start()
         {
             var consumer = new EventingBasicConsumer(_channel);
-            var limitReached = false;
-            void ConsumerOnReceived(object? sender, BasicDeliverEventArgs e)
+
+            var received = Observable.FromEventPattern<BasicDeliverEventArgs>(handler => consumer.Received += handler,
+                handler => consumer.Received -= handler);
+            if (_withTimeLimit) received = received.TakeUntil(Observable.Timer(_timeLimit));
+            if (_withCountLimit)
             {
-                if(limitReached) return;
+                var i = 0;
+                received = received.TakeWhile(pattern => i++ < _countLimit);
             }
-            consumer.Received += ConsumerOnReceived;
-            var tag = _channel.BasicConsume(consumer, "queuename", true, exclusive: true);
-            _limiter.OnLimit = () =>
+
+
+            var timer = new Timer {AutoReset = false, Interval = _timeLimit.TotalMilliseconds};
+                timer.Elapsed += (sender, args) => Stop();
+                consumer.Registered += (sender, args) => timer.Start();
+            }
+
+            if (_withCountLimit)
             {
-                limitReached = true;
-                _channel.BasicCancel(tag);
-                consumer.Received -= ConsumerOnReceived;
-            };
+                IDisposable? unsubscribe = null;
+                unsubscribe = received.Count(). ;.Subscribe(i =>
+                {
+                    if (i < _countLimit) return;
+                    Stop();
+                    unsubscribe?.Dispose();
+                });
+            }
+
+            _subscription = received.TimeInterval().Subscribe(OnMessage);
+            _limitReached = false;
+            
+            _consumerTag = _channel.BasicConsume(consumer, "queuename", true, exclusive: true);
+            //start limit and stats
+        }
+
+        private void OnMessage(TimeInterval<EventPattern<BasicDeliverEventArgs>> obj)
+        {
+            throw new NotImplementedException();
         }
 
 
         public void Stop()
         {
-            throw new NotImplementedException();
+            if (_consumerTag != null) _channel.BasicCancel(_consumerTag);
+            _subscription?.Dispose();
         }
     }
 }
