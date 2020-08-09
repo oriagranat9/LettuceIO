90c67eb1c2ee409e7b2d6602e69898274bd050b4
atlasyonatan <atlasxatlas@gmail.com>
2020-08-09 15:49:21 +0300

    WIP publish action

---
+++ LettuceIo.Dotnet/LettuceIo.Dotnet.Base/Actions/Publish.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Subjects;
+using System.Threading;
+using System.Threading.Tasks;
+using LettuceIo.Dotnet.Core;
+using RabbitMQ.Client;
+
+namespace LettuceIo.Dotnet.Base.Actions
+{
+    public class Publish : IAction
+    {
+        private readonly IConnectionFactory _connectionFactory;
+        private readonly Limits _limits;
+        private readonly string _exchange;
+        private readonly IReadOnlyCollection<Message> _messages;
+        private readonly PublishOptions _options;
+        public IObservable<ActionMetrics> Stats => _statsSubject;
+        private readonly ISubject<ActionMetrics> _statsSubject = new Subject<ActionMetrics>();
+        private bool _started;
+        private IModel? _channel;
+        private Random _random = new Random();
+        private bool _stop;
+        private CancellationTokenSource _cts = new CancellationTokenSource();
+
+        public Publish(IConnectionFactory connectionFactory, Limits limits, string exchange,
+            IReadOnlyCollection<Message> messages, PublishOptions options)
+        {
+            _connectionFactory = connectionFactory;
+            _limits = limits;
+            _exchange = exchange;
+            _messages = messages;
+            _options = options;
+        }
+
+        public void Start()
+        {
+            if (_started) throw new InvalidOperationException("The action was started already");
+            _started = true;
+            _channel = _connectionFactory.CreateConnection().CreateModel();
+
+            IEnumerable<Message> messages = _messages;
+            if (_options.Shuffle) messages = messages.Shuffle();
+            if (_options.Loop) messages = messages.Loop();
+             
+            if (_options.Playback)
+                Task.Run(() =>
+                {
+                    foreach (var message in messages)
+                    {
+                        if (_stop) return;
+                        Timer.Sleep(message.TimeDelta);
+                        BasicPublish(message);
+                    }
+                }, _cts.Token);
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Stop()
+        {
+            _stop = true;
+            _cts.Cancel();
+        }
+
+        private void BasicPublish(Message message) =>
+            _channel.BasicPublish(_exchange, message.RoutingKey, body: message.Body);
+    }
+}
---
+++ LettuceIo.Dotnet/LettuceIo.Dotnet.Base/EnumerableExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LettuceIo.Dotnet.Base
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> Loop<T>(this IEnumerable<T> enumerable)
+        {
+            var items = enumerable as T[] ?? enumerable.ToArray();
+            while (true)
+                foreach (var i in items)
+                    yield return i;
+        }
+
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> enumerable) => enumerable.Shuffle(new Random());
+
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> enumerable, Random rng) =>
+            enumerable.OrderBy(arg => rng.Next());
+    }
+}
