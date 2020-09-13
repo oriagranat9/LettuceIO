b65ed5de02b942376f94f9cee5cdce6c81b5b672
Unknown <oriagranat9@gmail.com>
2020-09-13 12:57:50 +0300

    redid select input with custom css
    now uses lists instead of the html select

--- src/components/Main/ActionWindow/Settings.vue
+++ src/components/Main/ActionWindow/Settings.vue
@@ -7,7 +7,7 @@
                     Folder
                 </button>
             </div>
-            <div class="col-5 align-self-center" style="max-width: 200px; text-align: start">
+            <div class="col-5 align-self-center" style="max-width: 300px; text-align: start">
                 <select-input v-model="selectedOption" :options="optionList" :text="selectName" style="margin: 0"/>
             </div>
         </div>
@@ -15,72 +15,76 @@
 </template>
 
 <script>
-import SelectInput from "../../inputs/selectInput";
+    import SelectInput from "../../inputs/selectInput";
 
-export default {
-    name: "Settings",
-    components: {SelectInput},
-    data() {
-        return {}
-    },
-    methods: {
-        selectFolder() {
-            const {dialog} = require('electron').remote;
-            dialog.showOpenDialog({properties: ['openDirectory', 'createDirectory']}).then(value => {
-                this.selectedFolder = value.filePaths[0];
-            })
+    export default {
+        name: "Settings",
+        components: {SelectInput},
+        data() {
+            return {}
+        },
+        methods: {
+            selectFolder() {
+                const {dialog} = require('electron').remote;
+                dialog.showOpenDialog({properties: ['openDirectory', 'createDirectory']}).then(value => {
+                    this.selectedFolder = value.filePaths[0];
+                })
+            }
+
+        },
+        computed: {
+            optionList: {
+                get() {
+                    let tmp = [];
+                    for (let i of this.$store.getters.getCurrentTab.tmpLists.optionList) {
+                        tmp.push(`${i['type']}/${i['name']}`)
+                    }
+                    return tmp;
+                }
+            },
+            selectName: {
+                get() {
+                    if (this.$store.getters.getCurrentTab.actionType === "Publish") {
+                        return "To";
+                    } else {
+                        return "From"
+                    }
+                }
+            },
+            selectedFolder: {
+                get() {
+                    return this.$store.getters.getCurrentTab.folderPath;
+                },
+                set(value) {
+                    this.$store.commit('setTabValue', {key: "folderPath", value: value});
+                }
+            },
+            selectedOption: {
+                get() {
+                    let selected = this.$store.getters.getCurrentTab.selectedOption;
+                    if (selected.name !== "" && selected.type !== "") {
+                        return `${selected['type']}/${selected['name']}`
+                    } else {
+                        return ""
+                    }
+                },
+                set(value) {
+                    let index = this.optionList.indexOf(value);
+                    let selectedOption = this.$store.getters.getCurrentTab.tmpLists.optionList[index];
+                    this.$store.commit('setTabValue', {key: "selectedOption", value: selectedOption});
+                }
+            },
         }
-
-    },
-    computed: {
-        optionList: {
-            get() {
-                let tmp = [];
-                for (let i of this.$store.getters.getCurrentTab.tmpLists.optionList) {
-                    tmp.push(`${i['type']}/${i['name']}`)
-                }
-                return tmp;
-            }
-        },
-        selectName: {
-            get() {
-                if (this.$store.getters.getCurrentTab.actionType === "Publish") {
-                    return "To";
-                } else {
-                    return "From"
-                }
-            }
-        },
-        selectedFolder: {
-            get() {
-                return this.$store.getters.getCurrentTab.folderPath;
-            },
-            set(value) {
-                this.$store.commit('setTabValue', {key: "folderPath", value: value});
-            }
-        },
-        selectedOption: {
-            get() {
-                let selected = this.$store.getters.getCurrentTab.selectedOption;
-                return `${selected['type']}/${selected['name']}`
-            },
-            set(value) {
-                let index = this.optionList.indexOf(value);
-                let selectedOption = this.$store.getters.getCurrentTab.tmpLists.optionList[index];
-                this.$store.commit('setTabValue', {key: "selectedOption", value: selectedOption});
-            }
-        },
     }
-}
 </script>
 
 <style scoped>
-.margins {
-    margin: 20px;
-}
+    .margins {
+        margin: 20px;
+    }
 
-.span {
-    width: 100% !important;
-}
+    .span {
+        width: 100% !important;
+    }
 
 </style>
--- src/components/inputs/selectInput.vue
+++ src/components/inputs/selectInput.vue
@@ -1,17 +1,20 @@
 <template>
-    <div class="form-group form-inline" style="white-space: nowrap">
-        <label :for="id" class="text-light justify-content-start" style="user-select: none; white-space: pre">{{text}}</label>
-        <select :id="id" class="form-control custom-select custom-select-sm text-light lettuce-input"
-                @input="$emit('input', $event.target.value)" :value="value">
-            <option v-for="option in options" v-bind:key="option">{{option}}</option>
-        </select>
+    <div class="form-group form-inline" style="white-space: nowrap; font-size: small;">
+        <label :for="id" class="text-light justify-content-start"
+               style="user-select: none; white-space: pre">{{text}}</label>
+        <v-select :options="options" :id="id" class="select-wrapper" @input="$emit('input', $event)" :value="value"/>
     </div>
 </template>
 
 <script>
+    import VSelect from '@alfsnd/vue-bootstrap-select'
+
     export default {
         name: "selectInput",
         props: ['text', 'value', 'options'],
+        components: {
+            VSelect
+        },
         data() {
             return {
                 id: null
@@ -23,11 +26,65 @@
     }
 </script>
 
-<style scoped>
-    label {
-        font-size: small;
-    }
-    select {
+<style>
+    .select-wrapper {
         width: 100% !important;
     }
+
+    /*Scroll bar css style for the select overflow*/
+    /* width */
+    .select-wrapper ::-webkit-scrollbar {
+        width: 5px;
+    }
+
+    /* Track */
+    .select-wrapper ::-webkit-scrollbar-track {
+        background: var(--bg-tertiary);
+    }
+
+    /* Handle */
+
+    .select-wrapper ::-webkit-scrollbar-thumb {
+        background: var(--brand-primary);
+        border-radius: 5px;
+    }
+
+    /*override over the default V select css */
+    .select-wrapper .v-select-toggle {
+        background-color: var(--bg-secondary) !important;
+        border-color: var(--brand-primary) !important;
+        color: #f8f9fa !important;
+    }
+
+    .select-wrapper .v-select-toggle:hover {
+        background-color: var(--bg-secondary) !important;
+        border-color: var(--brand-primary) !important;
+        color: #f8f9fa !important;
+    }
+
+    .select-wrapper .v-select-toggle:focus {
+        background-color: var(--bg-secondary) !important;
+        border-color: var(--brand-primary) !important;
+        color: #f8f9fa !important;
+        outline-color: var(--brand-secondary) !important;;
+    }
+
+    .select-wrapper .v-dropdown-container {
+        max-height: 40vmin;
+        overflow-y: auto;
+        overflow-x: hidden;
+        background-color: var(--bg-secondary) !important;
+    }
+
+    .select-wrapper .v-dropdown-item {
+        text-overflow: ellipsis;
+        overflow-x: hidden;
+        background-color: var(--bg-secondary) !important;
+        color: #f8f9fa !important;
+    }
+
+    .select-wrapper .v-dropdown-item:hover {
+        background-color: var(--brand-primary) !important;
+    }
+
 </style>
